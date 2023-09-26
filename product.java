d84f161319b68a28ce4bd1037dbc0295ca64633a
TejendraSinghChauhan <146069167+TejendraSinghChauhan@users.noreply.github.com>
2023-09-26 04:29:35 -0700

    Add files via upload

---
+++ product.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class product {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+       float rad = sc.nextFloat();
+       // int b = sc.nextInt();
+
+        float area = 3.14f*rad*rad;
+        System.out.println(area);
+    }
+
+    
+}
